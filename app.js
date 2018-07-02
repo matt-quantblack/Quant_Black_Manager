f5ce6d4177bfab7b31e3abc09949dfe38720ca18
Matthew Bailey <matthew.bailey@enzymelabs.com.au>
2018-07-02 15:20:55 +1000

    fixed issue with test_access

--- app.js
+++ app.js
@@ -65,5 +65,11 @@ if (fs.existsSync(filename)) {
     });
 }
 
-
+process.on('exit', function(code) {
+    // call the rest of the code and have it execute after 3 seconds
+    setTimeout(function() {
+        console.log("Exiting");
+    }, 3000);
+    return console.log(`About to exit with code ${code}`);
+});
 
